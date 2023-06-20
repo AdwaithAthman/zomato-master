32e89d7ae3454ed2e6bcf8f35f75f908d4ff0dde
AdwaithAthman <iamadwaith17@gmail.com>
2023-06-20 17:41:47 +0530

    Added google authentication

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,5 +1,6 @@
 //Library
 import express from "express";
+import passport from "passport";
 
 //Models
 import { UserModel } from "../../database/user";
@@ -43,14 +44,50 @@ Access      Public
 Method      POST
 */
 Router.post("/signin", async (req, res) => {
-    try{
-        const { email, password, fullName, phoneNumber } = req.body.credentials;
-        const user = await UserModel.findByEmailAndPassword(email, password);
-        const token = user.generateJwtToken();
-        return res.status(200).json({token, status: "Success"})
-    }catch(error){
-        return res.status(500).json({error: error.message})
-    }
+  try {
+    const { email, password, fullName, phoneNumber } = req.body.credentials;
+    const user = await UserModel.findByEmailAndPassword(email, password);
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "Success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
+
+/*
+Route       /auth/google
+Desc        Google signin
+Params      none
+Access      Public
+Method      GET
+*/
+Router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+/*
+Route       /auth/google/callback
+Desc        Google signin callback
+Params      none
+Access      Public
+Method      GET
+*/
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    return res.json({ user: req.session.passport.user.token });
+   /* return res.redirect(
+      `http://localhost:4000/google/${req.session.passport.user.token}`
+    );*/
+  }
+);
 
 export default Router;
